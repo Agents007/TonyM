62d91c78c07cd1a43ca90edf6021f0e15dfcf34b
gaeta <gaeta@DESKTOP-MET6QIB>
2021-07-13 20:42:28 +0200

    Ajoutez des fichiers projet.

---
+++ TonyM/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace TonyM
+{
+    class carteGraphique
+    {
+        string nom { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=fr-fr";
+            var webClient = new WebClient();
+
+            //webClient.DownloadFile(url, "nvidia.json");
+            //Console.WriteLine("OK");
+
+            var json = webClient.DownloadString(url);
+            json.
+            Console.WriteLine(json);
+        }
+    }
+}
