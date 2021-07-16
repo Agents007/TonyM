dddb370c127748d2fdb7dda063d91e8be136d4cf
Gaetan DEMAZEUX <gdemazeux@chevalier.fr>
2021-07-16 23:29:18 +0200

    160721

--- TonyM/Program.cs
+++ TonyM/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading;
 
 namespace TonyM
 {
@@ -84,6 +85,7 @@ namespace TonyM
 
             while (true)
             {
+                Console.Write("Votre choix : ");
                 string choice = Console.ReadLine();
                 try
                 {
@@ -97,7 +99,7 @@ namespace TonyM
                         else
                         {
                             gpus.Add(gpusVisible[choiceInt - 1]);
-                            Console.WriteLine(gpusVisible[choiceInt - 1] + " ajoutée à la liste, voulez vous ajouter une autre carte ?\n");
+                            Console.WriteLine(gpusVisible[choiceInt - 1] + " ajoutée à la liste. Ajoutez une autre carte ou terminé la sélection.\n");
                         }
                     }
                     else if ((choiceInt  == 10) && (gpus.Count > 0))
@@ -115,16 +117,46 @@ namespace TonyM
                 }
             }
 
-
-            Console.WriteLine("\nVotre sélection est la suivante :");
-            foreach (var gpu in gpus)
-            {
-                Console.WriteLine("- " + gpu);
-            }
-            Console.WriteLine("\nSi un gpu est disponible, Tony ouvrira la page web correspondante");
-
-
             return gpus;
+        }
+
+        static void OpenBuyPage(string link)
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = link;
+            System.Diagnostics.Process.Start(psi);
+        }
+
+        static void DisplayGpuWanted(List<string> gpuWanted)
+        {
+
+            Console.Write("VOTRE SELECTION -> ");
+            Console.WriteLine(String.Join(", ", gpuWanted));
+        }
+
+        static void SearchGpu(int refresh, List<CarteGraphique> gpus, List<string> gpusWanted)
+        {
+            while (true)
+            {
+                Thread.Sleep(refresh);
+                Console.Clear();
+                DisplayGpuWanted(gpusWanted);
+
+                var gpusFilter = gpus.Where(g => gpusWanted.Any(w => w == g.displayName)).ToList();
+
+                foreach (var gpu in gpusFilter)
+                {
+                    if (gpu.prdStatus != "out_of_stock")
+                    {
+                        OpenBuyPage(gpu.directPurchaseLink);
+                    }
+                    else
+                    {
+                        Console.WriteLine(gpu.displayName + " : " + gpu.prdStatus);
+                    }
+                }
+            }
         }
 
 
@@ -133,34 +165,35 @@ namespace TonyM
 
         static void Main(string[] args)
         {
-            const string url = "https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=fr-fr&category=GPU&manufacturer=NVIDIA&manufacturer_filter=NVIDIA~1";
-
+            const string url = "https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=fr-fr&category=GPU&gpu=RTX%203090,RTX%203080%20Ti,RTX%203080,RTX%203070%20Ti,RTX%203070,RTX%203060%20Ti,RTX%203060&gpu_filter=RTX%203090~12,RTX%203080%20Ti~7,RTX%203080~16,RTX%203070%20Ti~3,RTX%203070~18,RTX%203060%20Ti~8,RTX%203060~2,RTX%202080%20SUPER~1,RTX%202080~0,RTX%202070%20SUPER~0,RTX%202070~0,RTX%202060~6,GTX%201660%20Ti~0,GTX%201660%20SUPER~9,GTX%201660~8,GTX%201650%20Ti~0,GTX%201650%20SUPER~3,GTX%201650~17";
+            const int refresh = 3000;
 
             string json = getGpuFromNvidia(url);
             var gpus = GenerateGpu(json);
 
-            Console.WriteLine("Salut c'est Tony, j'ai des contacts dans la Mafia\n\nQuelle carte graphique recherche tu ?");
+            Console.WriteLine("Salut c'est Tony. J'ai des contacts dans la Mafia.\n\nQuelle carte graphique recherches tu ?");
 
             var gpusVisible = GetGpuVisible(gpus);
-
             var gpusWanted = GetGpuWanted(gpusVisible);
 
-            var gpusFilter = gpus.Where(g => gpusWanted.Any(w => w == g.displayName)).ToList();
+            Console.WriteLine();
+            Console.WriteLine("Merci, je consulte Laurent");
+            //DisplayGpuWanted(gpusWanted);
 
-
-            var uri = "https://www.google.com";
-            var psi = new System.Diagnostics.ProcessStartInfo();
-            psi.UseShellExecute = true;
-            psi.FileName = uri;
-            System.Diagnostics.Process.Start(psi);
+            SearchGpu(refresh, gpus, gpusWanted);
 
 
 
 
-            foreach (var gpu in gpusFilter)
-            {
-                Console.WriteLine(gpu.displayName + " - " + gpu.prdStatus + " - " + gpu.directPurchaseLink);
-            }
+
+
+
+
+            
+
+
+
+
 
 
         }
