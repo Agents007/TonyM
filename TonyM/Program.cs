05ba836a5e19bc63da67c83d3e9dfc979e0c0090
Gaetan DEMAZEUX <gdemazeux@chevalier.fr>
2021-08-03 12:34:33 +0200

    v2.1

--- TonyM/Program.cs
+++ TonyM/Program.cs
@@ -150,8 +150,8 @@ namespace TonyM
         {
             using WebClient webClient = new();
             webClient.Headers.Add("Accept", "application/json");
+            webClient.Headers.Add("cache-control", "no-cache, no-store, must-revalidate");
             webClient.Headers.Add("pragma", "no-cache");
-            webClient.Headers.Add("cache-control", "no-cache");
             webClient.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 OPR/77.0.4054.277");
             try
             {
