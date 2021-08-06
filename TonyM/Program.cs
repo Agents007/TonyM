5347df084dff6f413571e4433b99d0fddaf3eb83
Gaetan DEMAZEUX <gdemazeux@chevalier.fr>
2021-08-07 01:56:19 +0200

    v3

--- TonyM/Program.cs
+++ TonyM/Program.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
