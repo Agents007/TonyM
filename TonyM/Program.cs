8a448e36e3804c5b16563fcc1279656e4f4e3721
Gaetan DEMAZEUX <gdemazeux@chevalier.fr>
2021-08-21 00:31:11 +0200

    v3.1

--- TonyM/Program.cs
+++ TonyM/Program.cs
@@ -17,7 +17,6 @@ namespace TonyM
             const string URL_INIT = "https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=fr-fr&category=GPU&gpu=RTX%203090,RTX%203080%20Ti,RTX%203080,RTX%203070%20Ti,RTX%203070,RTX%203060%20Ti,RTX%203060&gpu_filter=RTX%203090~12,RTX%203080%20Ti~7,RTX%203080~16,RTX%203070%20Ti~3,RTX%203070~18,RTX%203060%20Ti~8,RTX%203060~2,RTX%202080%20SUPER~1,RTX%202080~0,RTX%202070%20SUPER~0,RTX%202070~0,RTX%202060~6,GTX%201660%20Ti~0,GTX%201660%20SUPER~9,GTX%201660~8,GTX%201650%20Ti~0,GTX%201650%20SUPER~3,GTX%201650~17";
             const string URL_BASE = "https://api.nvidia.partners/edge/product/search?page=1&limit=9&locale=fr-fr&category=GPU&gpu=";
             const int REFRESH = 1000;
-
             var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
             // ---------------Premier lancement de l'application-------------------------------------------------
@@ -38,8 +37,6 @@ namespace TonyM
             Console.WriteLine();
             Console.WriteLine("Merci, je consulte Laurent");
             // ----------------------Fin Initialisation--------------------------------------------
-
-
 
             // ---------------Recherche des RTX FE-------------------------------------------------       
             while (true)
